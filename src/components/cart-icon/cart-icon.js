9c616512035a41bfdb77137e88df2b07180fc3c3
puttapraveen <p.praveen.kumar.21.pk@gmail.com>
2020-08-03 23:29:07 +0530

    Items-Count added

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -4,14 +4,18 @@ import './cart-icon.scss'
 import { connect } from 'react-redux'
 import { toogleCartHidden } from '../../redux/cart/cart-action'
 
-const CardIcon=({toogleCartHidden})=>(
+const CardIcon=({toogleCartHidden,itemCount})=>(
 <div className='cart-icon' onClick={toogleCartHidden}>
     <ShoppingIcon className="shopping-icon"/>
-    <span className='item-count'>0</span>    
+<span className='item-count'>{itemCount}</span>    
 </div>
 )
+
+const mapStateToProps=({cart:{cartItems}})=>({
+    itemCount:cartItems.reduce((accumulatedQuantity,cartItem)=> accumulatedQuantity + cartItem.quantity,0)
+});
 
 const mapDispatchToProps=dispatch=>({
     toogleCartHidden:()=>dispatch(toogleCartHidden())
 })
-export default connect(null,mapDispatchToProps)(CardIcon)
+export default connect(mapStateToProps,mapDispatchToProps)(CardIcon)
