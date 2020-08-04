1f9218087c1c9b44931aa1c6f619b45c90216308
puttapraveen <p.praveen.kumar.21.pk@gmail.com>
2020-08-05 01:19:15 +0530

    clear items from checkout page added

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -19,6 +19,15 @@ const CartReducer=(state=INITIAL_STATE,action)=>{
                 ...state,
                 cartItems:addItemToCard(state.cartItems, action.payload)
             }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems:state.cartItems.filter(
+                    cartItem=>cartItem.id!==action.payload.id
+                )
+            }
+        
+
         default:
             return state
     }
