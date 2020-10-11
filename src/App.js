9d5434dfb8f80ba6d8ed217fea8b3ff2b726139e
puttapraveen <p.praveen.kumar.21.pk@gmail.com>
2020-10-12 02:55:52 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,71 +1,79 @@
-import React from 'react';
-import Homepage from "./pages/homepage/Homepage.js";
-import { connect } from 'react-redux'
-import { setCurrentUser } from './redux/user/user.action'
-import './App.css'
-import ShopPage from '../src/pages/shop/shop.js'
-import {Switch,Route,Redirect} from 'react-router-dom'
-import Header from '../src/components/header/header.js'
-import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.js'
-import {auth,createuserProfileDocument} from './firebase/firebase-utility';
-import { selectCurrentUser } from './redux/user/user-selector'
-import { createStructuredSelector } from 'reselect'
-import CheckoutPage from './pages/checkout/checkout'
+import React from "react";
+import "./App.css";
 
+import { Route, Switch, Redirect } from "react-router-dom";
 
-class App extends React.Component{
-   
-    unsubscribeFromAuth=null
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-    componentDidMount(){
-      this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
-        if(userAuth){
-          const userRef=await createuserProfileDocument(userAuth);
-          userRef.onSnapshot(snapShot=>{
-            this.props.setCurrentUser({
-                id:snapShot.id,
-                ...snapShot.data()
-              })
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
+import { setCurrentUser } from "./redux/user/user.actions";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
+import { selectCurrentUser } from "./redux/user/user.selectors";
+
+class App extends React.Component {
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // this.setState({ currentUser: user });
+      // createUserProfileDocument(user);
+      // console.log(user);
+
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data(),
           });
-        }
-        else{
-          this.props.setCurrentUser(userAuth)
-        }
-      });
-    }
+        });
+      } else {
+        setCurrentUser(userAuth);
+      }
+    });
+  }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-
-    componentWillUnmount(){
-      this.unsubscribeFromAuth();
-    }
-
-  render(){
-  return(
-   <div>
-     <Header/>
-     <Switch>
-    <Route exact path='/' component={Homepage} />
-    <Route path='/shop' component={ShopPage} />
-    <Route exact path='/checkout' component={CheckoutPage} />
-
-    <Route exact path='/sign' render={()=>
-      this.props.currentuser ? 
-      (<Redirect to='/' />):
-      (<SignInSignUp />)    
-    } />
-    </Switch>
-    </div>
- );
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signIn"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
-}
 
-const mapStateToProps=createStructuredSelector({
-  currentuser:selectCurrentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps=dispatch=>({
-  setCurrentUser:user=>dispatch(setCurrentUser(user))
-})
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,17 @@
-import { createStore , applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+// import { persistStore } from "redux-persist";
 
-import rootReducer from './root-reducer';
+import rootReducer from "./root-reducer";
 
-const middleware=[logger]
-const store=createStore(rootReducer,applyMiddleware(...middleware))
+// the middleware that the store is expecting from redux is going to be an array
+const middlewares = [logger];
 
+//createStore gets the reducer and the return value of apply middleware
+// applyMiddleware(...middlewares): what this will do is it will copy all the values in the array as individual arguments to the function applyMiddleware
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// const persistor = persistStore(store);
+
+// export default { store, persistor };
 export default store;
